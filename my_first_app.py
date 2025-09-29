52d2b0133ce4ba9abc3a6902c6d9a527714382b3
akm <akm2000@gmail.com>
2025-09-29 16:18:09 +0900

    ✨ Update database name and table name in app

--- my_first_app.py
+++ my_first_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-conn = st.connection("my_database")
-df = conn.query("select * from my_table")
+conn = st.connection("streamlit_db")
+df = conn.query("select * from users")
 st.dataframe(df)
